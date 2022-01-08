b165938735024059ae9edad05249eed66659469b
gyuta <kuroshiba0408@gmail.com>
2022-01-08 11:54:18 +0900

    orientの初期位置を修正

--- robel/dkitty/distral/dorient.py
+++ robel/dkitty/distral/dorient.py
@@ -2,6 +2,7 @@ import numpy as np
 import collections
 
 from robel.dkitty.distral.base import Base
+from robel.components.tracking import TrackerComponentBuilder, TrackerState
 
 class Orient(Base):
     def get_reward_dict(self, action, obs_dict):
@@ -23,3 +24,23 @@ class Orient(Base):
             ('bonus_big', 10 * ((angle_error < 0.087) * (upright > 0.96))),
         ))
         return reward_dict
+    
+    def _reset(self):
+        """Resets the environment."""
+        self._initial_angle = np.pi
+        # self._target_angle = np.pi
+
+        self._reset_dkitty_standing()
+        # Set the initial target position.
+        self.tracker.set_state({
+            'torso': TrackerState(
+                pos=np.zeros(3),
+                rot_euler=np.array([0, 0, self._initial_angle])),
+            # 'target': TrackerState(
+            #     pos=np.array([
+            #         # The D'Kitty is offset to face the y-axis.
+            #         np.cos(self._target_angle + np.pi / 2),
+            #         np.sin(self._target_angle + np.pi / 2),
+            #         0,
+            #     ])),
+        })
